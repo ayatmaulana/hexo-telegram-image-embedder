3f37d8c995ebb509eb2872c77eb14214431651ea
Ayat Maulana <ayatmaulana.04@gmail.com>
2018-06-03 06:41:19 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 
-const urlParser = require('parse-url')
+const urlParser = require('url-parse')
 
-hexo.extend.helper.register('telegram_image_embeder', function ( c ) {
+hexo.extend.tag.register('telegramImage', function (args) {
  
-    let parse = urlParser(c)
+    let parse = urlParser(args, true),
+        pathname = parse.pathname
     
-    return `<script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-post="${parse.pathname}" data-width="100%"></script>`
+    return `<script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-post="${pathname}" data-width="100%"></script>`
 
 });
